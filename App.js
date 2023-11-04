f2f1a43abec893234501997cc6745e12f2cb3703
VanshSh <vanshsharma9354@gmail.com>
2023-11-04 23:15:00 +0530

    feat: :sparkles: Video 16 => ActivityIndicator

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+} from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Learning React Native from Code Evolution.</Text>
-      <StatusBar style="auto" backgroundColor='red' barStyle="dark-content"  />
+    <View
+      style={{
+        flexDirection: 'column',
+        height: 100,
+        padding: 25,
+      }}
+    >
+      <View>
+        <Text
+          style={{
+            flexDirection: 'row',
+            height: 100,
+            padding: 30,
+          }}
+        >
+          Hello World!
+        </Text>
+      </View>
+      <View>
+        <ActivityIndicator size="large" color="red" animating />
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+})
