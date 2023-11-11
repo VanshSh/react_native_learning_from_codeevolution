660ba19f95b998f09cd06b9f53fe185a8e4dff46
VanshSh <vanshsharma9354@gmail.com>
2023-11-11 23:40:59 +0530

    feat: :sparkles: Video 57 => RNForms

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { SafeAreaView, StyleSheet, View } from 'react-native'
 import StylingRN from './components/StylingRN'
 import ListsRN from './components/ListsRN'
+import RNForms from "./components/RNForms"
 
 export default function App() {
   return (
     <SafeAreaView style={styles.padding20}>
       {/* <StylingRN/> */}
-      <ListsRN />
+      {/* <ListsRN /> */}
+      <RNForms/>
     </SafeAreaView>
   )
 }
