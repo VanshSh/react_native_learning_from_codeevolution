9121b79c8219fe5563a92c96ec9110f68e9062eb
VanshSh <vanshsharma9354@gmail.com>
2023-11-13 14:43:14 +0530

    feat: :sparkles: Video 65 => Form Validation

--- components/RNForm.js
+++ components/RNForm.js
@@ -13,7 +13,25 @@ import {
 
 const LoginForm = () => {
   const [userName, setUserName] = useState('')
-  const [password, setpassword] = useState('')
+  const [password, setPassword] = useState('')
+  const [errors, setErrors] = useState({})
+
+  const validateForm = () => {
+    let errors = {}
+    if (!userName) errors.userName = 'Name must not be empty !'
+    if (!password) errors.password = 'Password must not be empty !'
+    setErrors(errors)
+    return Object.keys(errors).length === 0
+  }
+
+  const handleFormSubmit = () => {
+    if (validateForm()) {
+      console.log('😇 L-29 in RNForm.js=> ', userName, password)
+      setUserName('')
+      setPassword('')
+      setErrors({})
+    }
+  }
   return (
     <SafeAreaView>
       <KeyboardAvoidingView
@@ -22,10 +40,10 @@ const LoginForm = () => {
         style={styles.container}
       >
         <View style={styles.form}>
-          <Image
+          {/* <Image
             source={require('../assets/adaptive-icon.png')}
             style={styles.image}
-          />
+          /> */}
           <Text style={styles.label}>Name</Text>
           <TextInput
             style={styles.input}
@@ -33,15 +51,22 @@ const LoginForm = () => {
             value={userName}
             onChangeText={setUserName}
           />
+          {errors.userName ? (
+            <Text style={styles.errorText}>{errors.userName}</Text>
+          ) : null}
           <Text style={styles.label}>Password</Text>
           <TextInput
             style={styles.input}
             placeholder='Enter your password..'
             secureTextEntry
             value={password}
-            onChangeText={setpassword}
+            onChangeText={setPassword}
           />
-          <Button title='Login' onPress={() => {}} />
+          {errors.password ? (
+            <Text style={styles.errorText}>{errors.password}</Text>
+          ) : null}
+
+          <Button title='Login' onPress={handleFormSubmit} />
         </View>
       </KeyboardAvoidingView>
     </SafeAreaView>
@@ -49,14 +74,14 @@ const LoginForm = () => {
 }
 
 const styles = StyleSheet.create({
-    container: {
+  container: {
     //   flex: 1,
-      paddingHorizontal: 20,
-      paddingVertical: 30,
-      backgroundColor: 'red',
-      borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    backgroundColor: 'red',
+    borderRadius: 10,
     //   justifyContent: 'center', // Adjust this based on your layout needs
-    },
+  },
   form: {
     backgroundColor: '#ffffff',
     padding: 20,
@@ -70,23 +95,23 @@ const styles = StyleSheet.create({
     shadowRadius: 3.84,
     elevation: 5,
   },
-    label: {
-      fontSize: 16,
-      marginBottom: 5,
-      fontWeight: 'bold',
-    },
-    input: {
-      height: 40,
-      borderColor: '#ddd',
-      borderWidth: 1,
-      marginBottom: 15,
-      padding: 10,
-      borderRadius: 5,
-    },
-    errorText: {
-      color: 'red',
-      marginBottom: 10,
-    },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+    fontWeight: 'bold',
+  },
+  input: {
+    height: 40,
+    borderColor: '#ddd',
+    borderWidth: 1,
+    marginBottom: 15,
+    padding: 10,
+    borderRadius: 5,
+  },
+  errorText: {
+    color: 'red',
+    marginBottom: 10,
+  },
   image: {
     width: 200,
     height: 400,
