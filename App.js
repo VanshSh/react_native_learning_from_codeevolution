5ccfb333542d93fe13b361ea0994a9255604b5ec
VanshSh <vanshsharma9354@gmail.com>
2023-11-14 21:55:09 +0530

    feat: :sparkles: Video 76 => Stack Navigation Options

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import HomeScreen from './screens/HomeScreen'
 import AboutScreen from './screens/AboutScreen'
+import { Pressable, Text } from 'react-native'
 
 export default function App() {
   const Stack = createStackNavigator()
@@ -12,12 +13,34 @@ export default function App() {
       {/* <RNFormsComponents /> */}
       {/* <RNForm/> */}
       {/* <RNNetworking /> */}
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator
+        initialRouteName='Home'
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#E5D4FF',
+          },
+          headerTintColor: '#363062',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerRight: () => (
+            <Pressable onPress={() => alert('Menu Pressed')}>
+              <Text>Menu</Text>
+            </Pressable>
+          ),
+          contentStyle: {
+            backgroundColor: '#192655',
+          },
+        }}
+      >
         <Stack.Screen
           name='Home'
           component={HomeScreen}
           initialParams={{
             result: 'Guest ',
+          }}
+          options={{
+            title: 'Welcome Home',
           }}
         />
         <Stack.Screen
--- notes.md
+++ notes.md
@@ -267,3 +267,51 @@ keyExtractor = {(item,index)=> item.id.toString()}
       const { name } = route.params
 
       ```
+    - Attributes: 
+    ```
+     <NavigationContainer>
+      {/* <ListsRN /> */}
+      {/* <StylingRN/> */}
+      {/* <RNFormsComponents /> */}
+      {/* <RNForm/> */}
+      {/* <RNNetworking /> */}
+      <Stack.Navigator
+        initialRouteName='Home'
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#E5D4FF',
+          },
+          headerTintColor: '#363062',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerRight: () => (
+            <Pressable onPress={() => alert('Menu Pressed')}>
+              <Text>Menu</Text>
+            </Pressable>
+          ),
+          contentStyle: {
+            backgroundColor: '#192655',
+          },
+        }}
+      >
+        <Stack.Screen
+          name='Home'
+          component={HomeScreen}
+          initialParams={{
+            result: 'Guest ',
+          }}
+          options={{
+            title: 'Welcome Home',
+          }}
+        />
+        <Stack.Screen
+          name='About'
+          component={AboutScreen}
+          initialParams={{
+            name: 'Guest login',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+    ```
