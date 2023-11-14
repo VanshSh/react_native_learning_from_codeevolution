e401dc4d92fa2c129e768a46f4c880d27ccf2422
VanshSh <vanshsharma9354@gmail.com>
2023-11-14 23:56:03 +0530

    feat: :sparkles: Video 81 => Tab Navigation

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
-import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native'
-import { createDrawerNavigator } from '@react-navigation/drawer'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { View, Text } from 'react-native'
 import React from 'react'
-import DashboardScreen from './screens/DashboardScreen'
-import SettingsScreen from './screens/SettingsScreee'
+import CourseScreen from './screens/CourseScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import SettingsScreen from './screens/SettingsScreen'
+import Ionicons from '@expo/vector-icons/Ionicons'
 
+const Tab = createBottomTabNavigator()
 const App = () => {
-  const Drawer = createDrawerNavigator()
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name='Dashboard' component={DashboardScreen} />
-        <Drawer.Screen name='Settings' component={SettingsScreen} />
-      </Drawer.Navigator>
+      <Tab.Navigator
+        screenOptions={{
+          //   tabBarShowLabel: false,
+          tabBarLabelPosition: 'below-icon',
+          tabBarActiveTintColor: 'purple',
+        }}
+      >
+        <Tab.Screen
+          name='Course List'
+          component={CourseScreen}
+          options={{
+            tabBarLabel: 'Course List',
+            tabBarIcon: () => <Ionicons name={'list'} size={20} />,
+          }}
+        />
+        <Tab.Screen
+          name='Profile'
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: 'My Profile',
+            tabBarIcon: () => <Ionicons name={'person'} size={20} />,
+            tabBarBadge: 3,
+          }}
+        />
+        <Tab.Screen
+          name='Settings'
+          component={SettingsScreen}
+          options={{
+            tabBarLabel: 'My Settings',
+            tabBarIcon: () => <Ionicons name={'settings'} size={20} />,
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
--- notes.md
+++ notes.md
@@ -316,7 +316,62 @@ keyExtractor = {(item,index)=> item.id.toString()}
     </NavigationContainer>
     ```
 
-## Video 78 Drawer Navigation
+## Video 78 Drawer Navigation (AppDrawer.js)
 
 - Read more here: https://reactnavigation.org/docs/drawer-navigator
-- 
+
+```
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { View, Text } from 'react-native'
+import React from 'react'
+import CourseScreen from './screens/CourseScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import SettingsScreen from './screens/SettingsScreen'
+import Ionicons from '@expo/vector-icons/Ionicons'
+
+const Tab = createBottomTabNavigator()
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          //   tabBarShowLabel: false,
+          tabBarLabelPosition: 'below-icon',
+          tabBarActiveTintColor: 'purple',
+        }}
+      >
+        <Tab.Screen
+          name='Course List'
+          component={CourseScreen}
+          options={{
+            tabBarLabel: 'Course List',
+            tabBarIcon: () => <Ionicons name={'list'} size={20} />,
+          }}
+        />
+        <Tab.Screen
+          name='Profile'
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: 'My Profile',
+            tabBarIcon: () => <Ionicons name={'person'} size={20} />,
+            tabBarBadge: 3,
+          }}
+        />
+        <Tab.Screen
+          name='Settings'
+          component={SettingsScreen}
+          options={{
+            tabBarLabel: 'My Settings',
+            tabBarIcon: () => <Ionicons name={'settings'} size={20} />,
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App
+
+
+```
