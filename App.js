f64a8314591ed7c0aa140c660391810c697f5e09
VanshSh <vanshsharma9354@gmail.com>
2023-11-14 14:32:34 +0530

    feat: :tada: Video 72 => React Native Navigation using (React Navigation)

--- App.js
+++ App.js
@@ -1,18 +1,14 @@
-import { SafeAreaView, StyleSheet, View } from 'react-native'
-import StylingRN from './components/StylingRN'
-import ListsRN from './components/ListsRN'
-import RNFormsComponents from './components/RNFormsComponents'
-import RNForm from './components/RNForm'
-import RNNetworking from './components/RNNetworking'
+import { NavigationContainer } from '@react-navigation/native'
 
 export default function App() {
   return (
-    <>
+    <NavigationContainer>
       {/* <ListsRN /> */}
       {/* <StylingRN/> */}
       {/* <RNFormsComponents /> */}
       {/* <RNForm/> */}
-      <RNNetworking />
-    </>
+      {/* <RNNetworking /> */}
+      <RNNavigation />
+    </NavigationContainer>
   )
 }
---
+++ components/RNNavigation.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const RNNavigation = () => {
+  return (
+    <SafeAreaView>
+      <Text>RNNavigation</Text>
+    </SafeAreaView>
+  )
+}
+
+export default RNNavigation
